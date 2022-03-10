34b9fd5b2db816d08fa170bf35b30d538c91c5b8
LongTranIT <tranphilongnvd@gmail.com>
2022-03-10 15:36:00 +0700

    Change port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const route=require('./routers');
 const app=express();
-const port=3000;
+const port= process.env.PORT || 3000;
 
 //Connect to database
 const db=require('./app/config/db');
