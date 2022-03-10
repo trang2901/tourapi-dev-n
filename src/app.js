13b41530cb195d38404ff2b636ec970d5e523a6c
LongTranIT <tranphilongnvd@gmail.com>
2022-03-10 19:27:20 +0700

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express=require('express');
 const route=require('./routers');
 const app=express();
+var cors = require('cors');
+ 
+app.use(cors());
 const port= process.env.PORT || 3000;
 
 //Connect to database
