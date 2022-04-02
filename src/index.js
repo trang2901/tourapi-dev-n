82d1c25e0164181af22b035b04cc2800446aa3ec
LongTranIT <tranphilongnvd@gmail.com>
2022-04-02 21:29:18 +0700

    add img

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const route=require('./routers');
 const app=express();
 var cors = require('cors');
+const path=require('path');
  
 app.use(cors());
 const port= process.env.PORT || 3000;
@@ -9,6 +10,9 @@ const port= process.env.PORT || 3000;
 //Connect to database
 const db=require('./app/config/db');
 db.connect();
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));//middleware
