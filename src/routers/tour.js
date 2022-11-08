79f3eeb94d2d246b11ee2c9ff35f62b98f285fbc
trang2901 <huyentrangst11b@gmail.com>
2022-11-08 15:39:51 +0700

    add

--- src/routers/tour.js
+++ src/routers/tour.js
@@ -2,11 +2,13 @@ const express=require('express');
 const router=express.Router();
 
 const TourController=require('../app/controllers/TourController');
+const Tour = require('../app/models/Tour');
 
 router.get('/',TourController.show);
 router.get('/page/:page',TourController.showPage);
 router.get('/allTags',TourController.showAllTags);
 router.get('/:slug',TourController.detail);
+router.get('/:mien', TourController.detail);
 router.post('/',TourController.create);
 router.put('/:id',TourController.update);
 router.patch('/:id',TourController.updatePatch);
