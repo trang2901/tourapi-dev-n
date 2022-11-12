cff59f8a74b433ebffd269a28129264eedbcfe25
trang2901 <huyentrangst11b@gmail.com>
2022-11-12 20:36:43 +0700

    update

--- src/app/controllers/MailController.js
+++ src/app/controllers/MailController.js
@@ -1,5 +1,49 @@
 
 const Mail = require('../models/Mail');
+const creds = require('../../credential.json');
+let nodemailer = require('nodemailer');
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", 
+      port: 587,
+      secure: false,
+    // service: 'Gmail',
+    auth: {
+      user: creds.auth.user,
+      pass: creds.auth.pass 
+    },
+  });
+
+// app.post('https://tourapi-dev-n.herokuapp.com/mail', (req, res, next) => {
+//       var email = req.body.email
+//       var message = req.body.message
+//       var subject = req.body.subject
+//       var name = req.body.name
+//       var company = req.body.company
+  
+//       const mailOptions = {
+//           from :  name,
+//           to : email,
+//           subject: subject,
+//           html: `${name} from ${company} <noreply@${name}.com> <br /><br /> ${message}`
+//       }
+  
+//       transporter.sendMail(mailOptions, (err, data) => {
+//           if(err){
+//               res.json({
+//                   status:"err"
+//               }) 
+//               console.log(err)
+//               }
+//               else {
+//                   res.json({
+//                       status: "success"
+//            })
+//            console.log("Email Sent" + data.response)
+//           }
+//       })
+//   })
+  
+  
 class MailController {
     // [GET] /diadiem
     show(req, res) {
@@ -13,16 +57,54 @@ class MailController {
 
     //[POST] /diadiem
     create(req, res) {
-        const mails = new Mail(req.body);
-        mails.save()
-            .then(data => {
-                res.json(data);
-            })
-            .catch(err => {
+    var email = req.body.email
+      var message = req.body.message
+      var subject = req.body.subject
+      var name = req.body.name
+      var company = req.body.company
+      const mailOptions = {
+        from :  name,
+        to : email,
+        subject: subject,
+        html: `${name} from ${company} <noreply@${name}.com> <br /><br /> ${message}`
+    }
+    transporter.sendMail(mailOptions, (err, data) => {
+        if(err){
+            res.json({
+                status:"err"
+            }) 
+            console.log(err)
+            }
+            else {
                 res.json({
-                    message: err
-                });
-            })
+                    status: "success"
+         })
+         console.log("Email Sent" + data.response)
+        }
+    })
+    transporter.verify(function(error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take our messages!");
+        }
+      });
+  console.log(app);
+  console.log(app.get(db), (req, res) =>
+      {
+          res.send(db);
+      });
+        // const mails = new Mail(req.body);
+        // mails.save()
+        //     .then(data => {
+        //         res.json(data);
+        //     })
+        //     .catch(err => {
+        //         res.json({
+        //             message: err
+        //         });
+        //     })
+
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const route=require('./routers');
 const app=express();
 var cors = require('cors');
 const path=require('path');
- 
+
+// const creds = require('./credential.json');
+// let nodemailer = require('nodemailer');
+
 app.use(cors());
 const port= process.env.PORT || 3000;
 
@@ -18,15 +21,62 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));//middleware
 
+// let transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com", 
+//       port: 587,
+//       secure: false,
+//     // service: 'Gmail',
+//     auth: {
+//       user: creds.auth.user,
+//       pass: creds.auth.pass 
+//     },
+//   });
+  
+  // app.post('https://tourapi-dev-n.herokuapp.com/mail', (req, res, next) => {
+  //     var email = req.body.email
+  //     var message = req.body.message
+  //     var subject = req.body.subject
+  //     var name = req.body.name
+  //     var company = req.body.company
+  
+  //     const mailOptions = {
+  //         from :  name,
+  //         to : email,
+  //         subject: subject,
+  //         html: `${name} from ${company} <noreply@${name}.com> <br /><br /> ${message}`
+  //     }
+  
+  //     transporter.sendMail(mailOptions, (err, data) => {
+  //         if(err){
+  //             res.json({
+  //                 status:"err"
+  //             }) 
+  //             console.log(err)
+  //             }
+  //             else {
+  //                 res.json({
+  //                     status: "success"
+  //          })
+  //          console.log("Email Sent" + data.response)
+  //         }
+  //     })
+  // })
+  
+//   transporter.verify(function(error, success) {
+//       if (error) {
+//         console.log(error);
+//       } else {
+//         console.log("Server is ready to take our messages!");
+//       }
+//     });
 // console.log(app);
-console.log(app.get(db), (req, res) =>
-    {
-        res.send(db);
-    });
+// console.log(app.get(db), (req, res) =>
+//     {
+//         res.send(db);
+//     });
 
 
 //Init Router
 route(app);
-
 app.listen(port);
 
