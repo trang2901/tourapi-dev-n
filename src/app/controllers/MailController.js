71d3613b4817624831e493a54250ee27e3c2776b
trang2901 <huyentrangst11b@gmail.com>
2022-11-12 23:09:11 +0700

    oussh

--- src/app/controllers/MailController.js
+++ src/app/controllers/MailController.js
@@ -61,46 +61,47 @@ class MailController {
         const mails = new Mail(req.body);
         mails.save()
             .then(data => {
-                const mailOptions = {
-                    from : 'DorisTour',
-                    to : req.body.email,
-                    subject:'DORISTOUR ĐÃ TẠO ĐƠN CỦA ANH/CHỊ',
-                    html: `
-                    DorisTour kính chào quý khách!<br />
-                    Rất cảm ơn quý khách hàng đã cho chúng tôi cơ hội được phục vụ.<br /><br />
-                    DorisTour đã tiếp nhận đơn đặt tour của bạn. <br />
-                    Sẽ mất 1-2 tiếng làm việc để chúng tôi kiểm tra và đối soát đơn của bạn và xác nhận đơn của bạn trong thời gian sớm nhất. <br />
-                    <b>Xin quý khách lưu ý: </b> Đây là Email xác nhận Đơn đặt tour của quý khách! Vui lòng không trả lời mail này. <br />
-                    Nếu bạn có bất kỳ thắc mắc nào, xin vui lòng liên hệ với chúng tôi qua đường dây nóng: 0394075201
-                    <br/>
-                    Trân trọng,
-                    <br />
-                    <b>DorisTour Team</b>
+                //gửi mail
+                // const mailOptions = {
+                //     from : 'DorisTour',
+                //     to : req.body.email,
+                //     subject:'DORISTOUR ĐÃ TẠO ĐƠN CỦA ANH/CHỊ',
+                //     html: `
+                //     DorisTour kính chào quý khách!<br />
+                //     Rất cảm ơn quý khách hàng đã cho chúng tôi cơ hội được phục vụ.<br /><br />
+                //     DorisTour đã tiếp nhận đơn đặt tour của bạn. <br />
+                //     Sẽ mất 1-2 tiếng làm việc để chúng tôi kiểm tra và đối soát đơn của bạn và xác nhận đơn của bạn trong thời gian sớm nhất. <br />
+                //     <b>Xin quý khách lưu ý: </b> Đây là Email xác nhận Đơn đặt tour của quý khách! Vui lòng không trả lời mail này. <br />
+                //     Nếu bạn có bất kỳ thắc mắc nào, xin vui lòng liên hệ với chúng tôi qua đường dây nóng: 0394075201
+                //     <br/>
+                //     Trân trọng,
+                //     <br />
+                //     <b>DorisTour Team</b>
     
-                    `
-                }
-                transporter.sendMail(mailOptions, (err, data) => {
-                    if(err){
-                        res.json({
-                            status:"err"
-                        }) 
-                        console.log(err)
-                        }
-                        else {
-                            res.json({
-                                status: "success"
-                     })
-                     console.log("Email Sent" + data.response)
-                    }
-                })
+                //     `
+                // }
+                // transporter.sendMail(mailOptions, (err, data) => {
+                //     if(err){
+                //         res.json({
+                //             status:"err"
+                //         }) 
+                //         console.log(err)
+                //         }
+                //         else {
+                //             res.json({
+                //                 status: "success"
+                //      })
+                //      console.log("Email Sent" + data.response)
+                //     }
+                // })
             
-                transporter.verify(function(error, success) {
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log("Server is ready to take our messages!");
-                    }
-                  });
+                // transporter.verify(function(error, success) {
+                //     if (error) {
+                //       console.log(error);
+                //     } else {
+                //       console.log("Server is ready to take our messages!");
+                //     }
+                //   });
                 res.json(data);
             })
             .catch(err => {
