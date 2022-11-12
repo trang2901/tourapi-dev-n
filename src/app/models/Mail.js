9177dd36427e7d8dcb3fbe295fcbda1440ae6ac8
trang2901 <huyentrangst11b@gmail.com>
2022-11-12 19:52:43 +0700

    kkk

--- src/app/models/Mail.js
+++ src/app/models/Mail.js
@@ -1,5 +1,4 @@
 const mongoose=require('mongoose');
-const slug = require('mongoose-slug-generator');
 const Schema=mongoose.Schema;
 const Mail=new Schema({
     name: String,
@@ -7,12 +6,9 @@ const Mail=new Schema({
     message: String,
     subject: String, 
     company: String
-
 },{
     timestamps:true
 })
-
-mongoose.plugin(slug);
 
 module.exports=mongoose.model('mail',Mail);
 
