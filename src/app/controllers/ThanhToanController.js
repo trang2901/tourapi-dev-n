fa08b65d6ca46e75d468ff2de77d8b7956eb614e
trang2901 <huyentrangst11b@gmail.com>
2022-11-12 23:27:27 +0700

    update

--- src/app/controllers/ThanhToanController.js
+++ src/app/controllers/ThanhToanController.js
@@ -35,9 +35,6 @@ class ThanhToanController {
         thanhToan.save()
             .then(data => {
                 res.json(data);
-
-                
-                
             })
             .catch(err => {
                 res.json({
