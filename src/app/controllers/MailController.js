4807bdbc1abf34a5c383e5d65ace1181f48ba5b4
trang2901 <huyentrangst11b@gmail.com>
2022-11-12 22:32:21 +0700

    push

--- src/app/controllers/MailController.js
+++ src/app/controllers/MailController.js
@@ -1,5 +1,6 @@
 
 const Mail = require('../models/Mail');
+
 const creds = require('../../credential.json');
 let nodemailer = require('nodemailer');
 let transporter = nodemailer.createTransport({
@@ -55,64 +56,64 @@ class MailController {
             });
     }
 
-    //[POST] /diadiem
+    //[POST] /mail
     create(req, res) {
-        // const mails = new Mail(req.body);
-        // mails.save()
-        //     .then(data => {
-        //         res.json(data);
-        //     })
-        //     .catch(err => {
-        //         res.json({
-        //             message: err
-        //         });
-        //     })
+        const mails = new Mail(req.body);
+        mails.save()
+            .then(data => {
+                const mailOptions = {
+                    from : 'DorisTour',
+                    to : req.body.email,
+                    subject:'DorisTour đã tạo đơn hàng của Anh/Chị',
+                    html: `
+                    <p style={{color: 'red'}}>Kính chào quý khách!</p><br />
+                    Email này được gửi từ Doristour!<br /><br />
+                    'Cảm ơn bạn đã cho chúng tôi cơ hội được phục vụ!'
+                    `
+                }
+                transporter.sendMail(mailOptions, (err, data) => {
+                    if(err){
+                        res.json({
+                            status:"err"
+                        }) 
+                        console.log(err)
+                        }
+                        else {
+                            res.json({
+                                status: "success"
+                     })
+                     console.log("Email Sent" + data.response)
+                    }
+                })
+            
+                transporter.verify(function(error, success) {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log("Server is ready to take our messages!");
+                    }
+                  });
+                res.json(data);
+            })
+            .catch(err => {
+                res.json({
+                    message: err
+                });
+            })
+
+
+
+
     //   var email = req.body.email
     //   var message = req.body.message
     //   var subject = req.body.subject
     //   var name = req.body.name
     //   var company = req.body.company
-
-      const mailOptions = {
-        from : 'DorisTour',
-        to : req.body.email,
-        subject:'DorisTour đã tạo đơn hàng của Anh/Chị',
-        html: `
-        <p style={{color: 'red'}}>Kính chào quý khách!</p><br />
-        Email này được gửi từ Doristour!<br /><br />
-        'Cảm ơn bạn đã cho chúng tôi cơ hội được phục vụ!'
-        `
-    }
-
-    transporter.sendMail(mailOptions, (err, data) => {
-        if(err){
-            res.json({
-                status:"err"
-            }) 
-            console.log(err)
-            }
-            else {
-                res.json({
-                    status: "success"
-         })
-         console.log("Email Sent" + data.response)
-        }
-    })
-    transporter.verify(function(error, success) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Server is ready to take our messages!");
-        }
-      });
-  console.log(app);
-  console.log(app.get(db), (req, res) =>
-      {
-          res.send(db);
-      });
-
-        
-
+//   console.log(app);
+//   console.log(app.get(db), (req, res) =>
+//       {
+//           res.send(db);
+//       });
     }
 
 
